25fb05cb64df467810cd2711b01a1fc8f54d1135
Saad Malik <36926771+maliksh7@users.noreply.github.com>
2022-07-08 02:38:09 +0500

    all other project files uploaded!

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  get 'home/index'
+  root 'home#index'
+
+  get 'home/about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # rails routes --> to view page routes
+end
